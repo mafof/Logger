14756f59fa886499e22b7a2dff1f7b262528ede2
mafof <pavel.bykov@mafof.ru>
2019-07-19 19:21:03 +0300

    init commit

---
+++ src/Log.php
@@ -0,0 +1,53 @@
+<?php
+namespace Logger;
+
+class Log {
+    private $settings;
+
+    public function __construct(Settings $settings = null) {
+        if($settings === null) $settings = new Settings();
+        $this->settings = $settings;
+    }
+
+    public function __call($type, $arguments) {
+        if(count($arguments) > 1 || count($arguments) === 0 || empty($arguments[0])) return false;
+
+        $message = $arguments[0];
+        $namePrefix = $this->settings->getPrefixName($type);
+
+        if($namePrefix !== false) {
+            if(gettype($message) === 'object' || gettype($message) === 'array') {
+                $this->log($namePrefix, var_export($message, true));
+            } else if(gettype($message) === 'string') {
+                $this->log($namePrefix, $message);
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private function log($prefixName, $message) {
+        $messageOutput = "";
+        if($this->settings->getIsTimestamp()) {
+            $messageOutput .= date($this->settings->getFormatTime());
+        }
+
+        $messageOutput .= " [$prefixName]";
+        $messageOutput .= " $message";
+        $messageOutput .= PHP_EOL;
+
+        echo $messageOutput;
+        $this->writeFile($messageOutput);
+    }
+
+    private function writeFile($message) {
+        $fileName = $this->settings->getNameFileLogs() . "_" . date("Y.m.d") . '.log';
+
+        if(file_exists($this->settings->getPathFileName() . DIRECTORY_SEPARATOR . $fileName)) {
+            file_put_contents($this->settings->getPathFileName() . DIRECTORY_SEPARATOR . $fileName, $message, FILE_APPEND | LOCK_EX);
+        } else {
+            mkdir($this->settings->getPathFileName());
+            file_put_contents($this->settings->getPathFileName() . DIRECTORY_SEPARATOR . $fileName, $message, LOCK_EX);
+        }
+    }
+}
---
+++ src/Settings.php
@@ -0,0 +1,95 @@
+<?php
+namespace Logger;
+
+class Settings {
+    private $pathFileLogs = './logs';
+    private $nameFileLogs = 'logs';
+
+    private $isTimestamp = true;
+    private $formatTimestamp = "Y-m-d H:i:s";
+
+    private $listPrefix = [
+        'info'  => "INFORMATION",
+        'warn'  => "WARNING",
+        'error' => "ERROR"
+    ];
+
+    public function __construct() {}
+
+    public function setPathFileLog($path) {
+        if(is_string($path)) {
+            $this->pathFileLogs = $path;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setNameFileLog($name) {
+        if(is_string($name)) {
+            $this->nameFileLogs = $name;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setFormatTime($format) {
+        if(is_string($format)) {
+            $this->formatTimestamp = $format;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setIsTimestamp($isTimestamp) {
+        if(is_bool($isTimestamp)) {
+            $this->isTimestamp = $isTimestamp;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function addPrefix($type, $value) {
+        if((is_string($type) && is_string($value)) && !array_key_exists($type, $this->listPrefix)) {
+            $this->listPrefix[$type] = $value;
+            return true;
+        } else {
+           return false;
+        }
+    }
+
+    public function setPrefix($type, $value) {
+        if((is_string($type) && is_string($value)) && array_key_exists($type, $this->listPrefix)) {
+            $this->listPrefix[$type] = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function removePrefix($type) {
+        if(array_key_exists($type, $this->listPrefix)) {
+            unset($this->listPrefix[$type]);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getPrefixName($type) {
+        if(array_key_exists($type, $this->listPrefix)) {
+            return $this->listPrefix[$type];
+        } else {
+            return false;
+        }
+    }
+
+    public function getFormatTime() { return $this->formatTimestamp; }
+    public function getIsTimestamp() { return $this->isTimestamp; }
+    public function getListPrefix() { return $this->listPrefix; }
+    public function getNameFileLogs() { return $this->nameFileLogs; }
+    public function getPathFileName() { return $this->pathFileLogs; }
+}
